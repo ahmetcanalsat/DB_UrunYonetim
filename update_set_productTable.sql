852e1d44753134ecff5643dec016c36d70eea694
Ahmet Can ALSAT <ahmetcan_045@hotmail.com>
2023-04-04 02:56:05 +0300

    Update_komutu
    update komutu ile ID'si 2 olan ürünün alış fiyatını ve stoğunu güncelledik.

---
+++ update_set_productTable.sql
@@ -0,0 +1,4 @@
+﻿use DbUrunYonetim
+
+/*UPDATE tblProduct SET prod_buyPrice=1950, prod_Stock=80 WHERE prod_ID=2*/
+/*Yukarıdaki komutumuz ile Product tablomuzdaki ID'si 2 olan satırın buyPrice'ını 1950 ve Stock kısmını 80 yaptık.
