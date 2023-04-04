7e85e7322efdae7030e2da73cf4a426192eb0d27
Ahmet Can ALSAT <ahmetcan_045@hotmail.com>
2023-04-04 03:02:49 +0300

    update komutu örnek
    update komutu elimizdeki laptopların stoklarına +10 ekledik.

--- update_set_productTable.sql
+++ update_set_productTable.sql
@@ -1,4 +1,7 @@
 ﻿use DbUrunYonetim
 
 /*UPDATE tblProduct SET prod_buyPrice=1950, prod_Stock=80 WHERE prod_ID=2*/
-/*Yukarıdaki komutumuz ile Product tablomuzdaki ID'si 2 olan satırın buyPrice'ını 1950 ve Stock kısmını 80 yaptık.
+/*Yukarıdaki komutumuz ile Product tablomuzdaki ID'si 2 olan satırın buyPrice'ını 1950 ve Stock kısmını 80 yaptık.*/
+
+/*UPDATE tblProduct SET prod_Stock=prod_Stock+10 WHERE prod_Name='Laptop'*/
+/*Yukarıdaki komutumuz ile Product tablomuzdaki adı Laptop olan tüm ürünlerin stoğuna +10 ekledik.
