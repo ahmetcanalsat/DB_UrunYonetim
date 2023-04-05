2bf3f6306433dbf0822b3e45c8da4b3300e5b0e8
Ahmet Can ALSAT <ahmetcan_045@hotmail.com>
2023-04-05 22:51:34 +0300

    Data entry to employee table
    We have added data to the employees table

---
+++ insert_into_personalTable.sql
@@ -0,0 +1,11 @@
+﻿use DbUrunYonetim
+
+INSERT INTO tblPersonal(pers_Name,pers_Department,pers_Salary,pers_Status,pers_City) VALUES ('Selda','Muhasebe',3800,'True','Ankara')
+INSERT INTO tblPersonal(pers_Name,pers_Department,pers_Salary,pers_Status,pers_City) VALUES ('Sinem','Satın Alma',4500,'True','Ankara')
+INSERT INTO tblPersonal(pers_Name,pers_Department,pers_Salary,pers_Status,pers_City) VALUES ('Kadriye','Satın Alma',4500,'True','Çanakkale')
+INSERT INTO tblPersonal(pers_Name,pers_Department,pers_Salary,pers_Status,pers_City) VALUES ('Yunus','Satın Alma',4500,'True','İstanbul')
+INSERT INTO tblPersonal(pers_Name,pers_Department,pers_Salary,pers_Status,pers_City) VALUES ('Ömer','Danışma',6000,'True','Balıkesir')
+INSERT INTO tblPersonal(pers_Name,pers_Department,pers_Salary,pers_Status,pers_City) VALUES ('Funda','Muhasebe',5000,'True','Nevşehir')
+INSERT INTO tblPersonal(pers_Name,pers_Department,pers_Salary,pers_Status,pers_City) VALUES ('Burak','Saha Sorumlusu',4000,'True','Trabzon')
+INSERT INTO tblPersonal(pers_Name,pers_Department,pers_Salary,pers_Status,pers_City) VALUES ('Eylül','Mutfak',4000,'False','Bursa')
+INSERT INTO tblPersonal(pers_Name,pers_Department,pers_Salary,pers_Status,pers_City) VALUES ('Mesut','Koordinatör',5200,'True','İstanbul')
