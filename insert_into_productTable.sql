4c272af78b4ab1f2c738341b1956a5ac6ab920b4
Ahmet Can ALSAT <ahmetcan_045@hotmail.com>
2023-04-12 05:11:15 +0300

    new insert_into_productTable
    Ürünler tablomuza yeni bir satır ekledik ve kategori sütunu ile kategori_ID sütunumuzu ilişkilendirdik.

--- insert_into_productTable.sql
+++ insert_into_productTable.sql
@@ -1,5 +1,5 @@
 ﻿use DbUrunYonetim
-INSERT INTO tblProduct(prod_Name,prod_Code,prod_buyPrice,prod_sellPrice,prod_Stock,prod_Category)
+/*INSERT INTO tblProduct(prod_Name,prod_Code,prod_buyPrice,prod_sellPrice,prod_Stock,prod_Category)
 VALUES
 ('Çamaşır Makinesi','CM1', 1800.00, 2200.00, 70, 1),
 ('Laptop','LP1', 5400.00, 6000.00, 20 , 4),
@@ -9,4 +9,7 @@ VALUES
 ('Ütü','UT1', 140.00, 180.00, 100, 3),
 ('Bulaşık Makinesi','BM1', 1250.00, 1500.00, 50, 1),
 ('Buzdolabı','BZ3', 3500.00, 3850.00, 40, 1),
-('Tablet', 'TB1', 450.00, 700.00, 30, 3)
+('Tablet', 'TB1', 450.00, 700.00, 30, 3)*/
+
+/*INSERT INTO tblProduct(prod_Name,prod_Code,prod_buyPrice,prod_sellPrice,prod_Stock,prod_Category)
+VALUES('Ütü','UT1',130,160,50,3)*/
