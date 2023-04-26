8bdf8e6710e7a2713a87c9179e893ad74144c9df
Ahmet Can ALSAT <ahmetcan_045@hotmail.com>
2023-04-26 15:33:03 +0300

    null_degerli_veriGirisi
    Null değerli veriler nasıl girilir ve çoklu satır silmeyi örneklerle gösterdik ve öğrendik.

---
+++ insert_intoANDnull_personalTable.sql
@@ -0,0 +1,11 @@
+﻿USE DbUrunYonetim
+
+/*INSERT INTO tblPersonal(pers_Name,pers_Department,pers_Salary,pers_Status,pers_City)
+VALUES 
+('Turgay',NULL,4300,'False',NULL),
+('Murat','Satın Alma',5400,NULL,'Adana'),
+('Ceren',NULL,4200,'True',NULL)*/
+/*Yukarıdaki sorgumuzla tablomuza NULL değerleri eklemeyi öğrendik.*/
+
+/*DELETE FROM tblPersonal WHERE pers_ID >=16 AND pers_ID <=18 */
+/*Yukarıdaki delete sorgumuz ile belirli satırları çoklu silebilmeyi öğrendik.*/
